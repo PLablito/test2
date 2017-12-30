e1d9d91bec54b2ceb3780aabdc0069344a9e1514
PLablito <pawelberbeka@onet.pl>
2017-12-30 18:19:05 +0100

    files added

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var sass = require('gulp-sass');
+/* var sourcemaps = require('gulp-sourcemaps'); */
+
+
+gulp.task('sass', function () {
+    var plugins = [
+        autoprefixer({browsers: ['last 2 versions', 'ie >= 9']})
+    ];
+    return gulp.src('./sass/*.scss')
+        /* .pipe(sourcemaps.init()) */
+        .pipe(sass().on('error', sass.logError))
+        /*
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+         */
+        /* .pipe(sourcemaps.write()) */
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./css'))
+
+});
+
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
